33e720d171a5c369d820e6d87bb2650dcdac258b
Zeynep Ejder <zeynepejder@dyn-160-39-251-168.dyn.columbia.edu>
2016-04-20 23:50:30 -0400

    Scaling

--- FinalProject4172/Assets/Scripts/L_Shape.cs
+++ FinalProject4172/Assets/Scripts/L_Shape.cs
@@ -6,6 +6,7 @@ public class L_Shape : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		transform.position = new Vector3 (0, 5, 0);
+		transform.localScale = new Vector3 (0.2f, 0.2f, 0.2f);
 	
 	}
 	
