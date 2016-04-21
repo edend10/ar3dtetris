d659bfd2270f0961593641093dc193b88fb85585
edolev89 <edolev89@gmail.com>
2016-04-20 23:34:21 -0400

    the beginning of a beautiful rotation

---
+++ FinalProject4172/Assets/Scripts/BrickControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrickControl : MonoBehaviour {
+
+	GameObject activeBrick;
+
+	// Use this for initialization
+	void Start () {
+		activeBrick = GameObject.Find ("tempbrick"); //temp
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("q")){ 
+			activeBrick.transform.Rotate (90,0,0);
+		}
+		else if(Input.GetKeyDown("a")){
+			activeBrick.transform.Rotate (-90,0,0);
+		}
+		else if(Input.GetKeyDown("w")){
+			activeBrick.transform.Rotate (0,90,0);
+		}
+		else if(Input.GetKeyDown("s")){
+			activeBrick.transform.Rotate (0,-90,0);
+		}
+		else if(Input.GetKeyDown("e")){
+			activeBrick.transform.Rotate (0,0,90);
+		}
+		else if(Input.GetKeyDown("d")){
+			activeBrick.transform.Rotate (0,0,-90);
+		}
+
+	}
+}
