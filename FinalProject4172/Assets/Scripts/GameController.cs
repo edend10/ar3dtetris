d996ac9c8a934cf4bdf2b2bed1fe3e3de7d2838e
Zeynep Ejder <zeynepejder@dyn-160-39-251-168.dyn.columbia.edu>
2016-04-20 22:12:58 -0400

    Scripts

---
+++ FinalProject4172/Assets/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject I;
+	public GameObject L;
+	public GameObject S;
+	public GameObject T;
+	public GameObject Square;
+
+	public GameObject ground;
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void createBrick(){
+		System.Random r = new System.Random ();
+		int number = r.Next (1, 6);
+		GameObject newObject;
+		if (number == 1) {
+			newObject = Instantiate (I);
+		}
+
+
+	}
+}
