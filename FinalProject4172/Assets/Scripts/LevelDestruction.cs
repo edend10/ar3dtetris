237e69ee34b9fabcc27687ef4ef174f25c60e361
lrntm <lmt2172@barnard.edu>
2016-04-24 16:38:53 -0400

    level destruction updated not tested

---
+++ FinalProject4172/Assets/Scripts/LevelDestruction.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelDestruction : MonoBehaviour {
+
+	static int blockCount;
+	ArrayList childBlocks = new ArrayList();
+
+
+
+	// Use this for initialization
+	void Start () {
+		blockCount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (blockCount == 25)
+			destroyLevel (); 
+	
+	}
+
+	void onCollisionEnter(Collision block) {
+
+		++blockCount;
+		childBlocks.Add (block.rigidbody);
+
+	}
+
+	void onCollisionExit(Collision block) {
+		--blockCount;
+		childBlocks.Remove (block.rigidbody);
+	}
+
+
+	void destroyLevel() {
+		
+	}
+}
