8ea69aeac23b6d5f9a0d4b158cb8d4679134c603
lrntm <lmt2172@barnard.edu>
2016-04-24 17:54:55 -0400

    level destruction updated not tested

--- FinalProject4172/Assets/Scripts/LevelDestruction.cs
+++ FinalProject4172/Assets/Scripts/LevelDestruction.cs
@@ -3,13 +3,12 @@ using System.Collections;
 
 public class LevelDestruction : MonoBehaviour {
 
-	ArrayList childBlocks = new ArrayList();
-
+	ArrayList<GameObject> childBlocks = new ArrayList();
+	GameObject currentChild;
 
 
 	// Use this for initialization
 	void Start () {
-		blockCount = 0;
 	}
 	
 	// Update is called once per frame
@@ -22,22 +21,22 @@ public class LevelDestruction : MonoBehaviour {
 
 	void onCollisionEnter(Collision block) {
 
-		childBlocks.Add (&block.rigidbody);
-		Debug.Log ("added " + block.ToString () + " size: " + childBlocks.Count);
+		childBlocks.Add (block.rigidbody.gameObject);
+		Debug.Log ("added " + block.rigidbody.gameObject.ToString () + " size: " + childBlocks.Count);
 
 	}
 
 	void onCollisionExit(Collision block) {
 		
-		childBlocks.RemoveAt (&childBlocks.Count);
-		Debug.Log ("removed " + block.ToString () + " size: " + childBlocks.Count);
+		childBlocks.RemoveAt (childBlocks.Count);
+		Debug.Log ("removed " + block.rigidbody.gameObject.ToString () + " size: " + childBlocks.Count);
 
 	}
 
 
 	void destroyLevel() {
 		for (int i = 0; i <= 25; ++i) {
-			Destroy (childBlocks [i]);
+			childBlocks [i].setActive (false); 
 		}
 	}
 }
