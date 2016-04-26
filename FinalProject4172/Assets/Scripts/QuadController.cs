f4bc537093174e709d8add73908ece9190edb572
cedricmayer <jhm2157@columbia.edu>
2016-04-26 17:21:29 -0400

    minimap cam scripts

---
+++ FinalProject4172/Assets/Scripts/QuadController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuadController : MonoBehaviour {
+
+	private Camera ARcam;
+	private Vector3 offset;
+
+	void Start () {
+
+		ARcam = GameObject.Find ("Camera").GetComponent<Camera> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		offset = ARcam.transform.forward * 5 + ARcam.transform.right * 2 + ARcam.transform.up ;
+		transform.position = ARcam.transform.position + offset;
+	}
+}
