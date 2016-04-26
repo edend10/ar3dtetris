602aaa1254c2205c5ae34e5b260f10f0824f65f2
Zeynep Ejder <zeynepejder@dyn-160-39-250-152.dyn.columbia.edu>
2016-04-26 14:27:39 -0400

    cursor

--- FinalProject4172/Assets/Scripts/mySelection.cs
+++ FinalProject4172/Assets/Scripts/mySelection.cs
@@ -1,15 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class mySelection : MonoBehaviour {
+public class mySelection : MonoBehaviour
+{
+	
+	GameObject hitObject;
+
+	GameObject selectedObject;
+
+	float startTime;
+
+	public GameObject ARcamera;
+	Transform cam;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		cam = Camera.main.transform;
 	}
-	
+		
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		
+		RaycastHit hit;
+		cam = Camera.main.transform;
+		Ray ray = new Ray (cam.position, cam.forward);
+
+		if (Physics.Raycast (ray, out hit)) {
+			GameObject o = hit.collider.gameObject;
+			if (o == selectedObject) {
+				
+			} else if (o == hitObject) {
+				//center.fontSize += 1;
+				float endTime = Time.time;
+				float time = endTime - startTime;
+				if (time > 2) {
+					Debug.Log ("Raycast: " + hit.collider.gameObject);
+					selectedObject = hitObject;
+					//center.color = Color.red;
+				}
+								
+			} else {
+				startTime = Time.time;
+				hitObject = o;
+			}
+		} else {
+			//center.fontSize = 12;
+			//center.color = Color.black;
+			hitObject = null;
+		}
+			
 	}
+			
 }
