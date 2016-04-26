a5b1a1ccff86c3f7959f451f394e308d4ffb2548
lrntm <lmt2172@barnard.edu>
2016-04-26 16:33:09 -0400

    updating level clear script

--- FinalProject4172/Assets/Scripts/LevelDestruction.cs
+++ FinalProject4172/Assets/Scripts/LevelDestruction.cs
@@ -14,27 +14,32 @@ public class LevelDestruction : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (childBlocks.Count == 25)
+		if (childBlocks.Count == 2)
 			destroyLevel (); 
 	
 	}
 
-	void onCollisionEnter(Collision block) {
-
-		childBlocks.Add (block.rigidbody.gameObject);
-		Debug.Log ("added " + block.rigidbody.gameObject.ToString () + " size: " + childBlocks.Count);
+	//void onCollisionEnter(Collision block) {
+	void onTriggerEnter(Collider block) {
+		Debug.Log ("entered");
+		//childBlocks.Add (block.rigidbody.gameObject);
+		childBlocks.Add(block.gameObject);
+		//Debug.Log ("added " + block.rigidbody.gameObject.ToString () + " size: " + childBlocks.Count);
+		Debug.Log ("added " + block.gameObject.ToString () + " size: " + childBlocks.Count);
 
 	}
 
-	void onCollisionExit(Collision block) {
-		
+	//void onCollisionExit(Collision block) {
+	void onTriggerExit (Collider block) {
+		Debug.Log ("exit");
 		childBlocks.RemoveAt (childBlocks.Count);
-		Debug.Log ("removed " + block.rigidbody.gameObject.ToString () + " size: " + childBlocks.Count);
+		//Debug.Log ("removed " + block.rigidbody.gameObject.ToString () + " size: " + childBlocks.Count);
+		Debug.Log ("removed " + block.gameObject.ToString () + " size: " + childBlocks.Count);
 	}
 
 
 	void destroyLevel() {
-		for (int i = 0; i <= 25; ++i) {
+		for (int i = 0; i <= 2; ++i) {
 			currentChild = (GameObject)childBlocks [i];
 			currentChild.SetActive (false);
 		}
