48d9a3df92f7f504084456366ec22116d5c9dd0d
Zeynep Ejder <zeynepejder@dyn-160-39-250-120.dyn.columbia.edu>
2016-04-30 17:25:46 -0400

    user movement

--- FinalProject4172/Assets/Scripts/UserControl.cs
+++ FinalProject4172/Assets/Scripts/UserControl.cs
@@ -5,6 +5,8 @@ public class UserControl : MonoBehaviour {
 
 	public GameObject head;
 
+	float fixedDistance = 10.0f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -12,6 +14,15 @@ public class UserControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		Vector3 targetPosition = gameObject.transform.position;
+		Vector3 headPosition = head.transform.position;
+
+		float z = Mathf.Abs(targetPosition.z - headPosition.z);
+
+		float displacement = z - fixedDistance;
+		Vector3 move = new Vector3 (0, 0, displacement);
+		head.transform.position += move;
 	
 	}
 }
