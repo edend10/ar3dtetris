30846e7c905e7de2d5c347e001b759cb9fa809a8
cedricmayer <jhm2157@columbia.edu>
2016-05-02 21:27:09 -0400

    better halo

--- FinalProject4172/Assets/Scripts/PowerUp.cs
+++ FinalProject4172/Assets/Scripts/PowerUp.cs
@@ -6,17 +6,27 @@ public class PowerUp : MonoBehaviour {
 	public bool isFalling;
 	public GameObject tL1, tL2, tL3;
 
+	private Light halo;
+	private bool powerReady;
+
 	void Start () {
 	
 		tL1 = GameObject.Find ("Level 1");
 		tL2 = GameObject.Find ("Level 2");
 		tL3 = GameObject.Find ("Level 3");
 
+		halo = GetComponent<Light>();
+
+		powerReady = true;
+
 	}
 	
 
 	void Update () {
 			
+		if (powerReady) {
+			Glow ();
+		}
 	}
 
 	public void youAreSelected(){
@@ -33,6 +43,11 @@ public class PowerUp : MonoBehaviour {
 	public void Destroy(GameObject g){
 
 		g.GetComponent<LevelDestruction> ().DestroyLevel ();
+	}
+
+	public void Glow(){
+
+		halo.range = 2;
 	}
 
 }
