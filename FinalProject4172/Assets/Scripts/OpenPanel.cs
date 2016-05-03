01d95fcfd9bc5c08be02af99f60a9fe038c006c2
Zeynep Ejder <zeynepejder@dyn-160-39-123-24.secure.barnard.edu>
2016-05-03 06:43:08 -0400

    game play

---
+++ FinalProject4172/Assets/Scripts/OpenPanel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenPanel : MonoBehaviour {
+
+	public GameObject panel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+		
+	public void youAreSelected(){
+		GameObject control = GameObject.Find ("GameController");
+
+		if (panel.activeSelf) {
+			panel.SetActive (false);
+			control.SendMessage ("pause", false);
+		} else {
+			panel.SetActive (true);
+			control.SendMessage ("pause", true);
+		}
+	}
+}
