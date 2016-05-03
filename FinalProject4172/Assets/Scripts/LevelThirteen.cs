d00e03d3affbe5aaf6936fd425d97553065405c9
lrntm <lmt2172@barnard.edu>
2016-05-03 13:22:29 -0400

    game over not working yet

---
+++ FinalProject4172/Assets/Scripts/LevelThirteen.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelThirteen : MonoBehaviour {
+
+	GameObject g;
+
+	float timeIn, timeOut, timeStay;
+
+	void Start() {
+
+	}
+
+	void Update() {
+		if (timeStay - timeIn > 2f) {
+			Debug.Log ("GAME OVER");
+			g.GetComponent<GameController> ().gameOver ();
+		}
+	}
+
+	void OnTriggerEnter (Collider block) {
+		timeIn = Time.time;
+	}
+	void OnTriggerExit (Collider block) {
+		timeOut = Time.time;
+	}
+	void OnTriggerStay (Collider block) {
+		timeStay = Time.time;
+	}
+}
