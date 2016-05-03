2a0263849c1fe45bdd82c364fb51594e550e7f28
lrntm <lmt2172@barnard.edu>
2016-05-02 23:32:30 -0400

    status bar + controller added

---
+++ FinalProject4172/Assets/Scripts/StatusBarController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatusBarController : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+		InvokeRepeating ("showBox", 1f, 1f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if (GameController.releaseTimer % 1 == 0) {
+//
+//			for (int i = 0; i <= GameController.releaseTimer; ++i) {
+//				showBox (i);
+//			}
+//		}
+//		if (GameController.releaseTimer % 1 == 0) {
+//			//removeBox ();
+//		}
+
+	}
+
+
+	void showBox() {
+		for (int i = 0; i <= GameController.releaseTimer; ++i) {
+			GameObject.Find ("StatusCube" + i).SetActive (true);
+			Debug.Log (i + " statuscube");
+		}
+	}
+
+	void removeBox(int i) {
+
+		GameObject.Find ("StatusCube" + i).SetActive (false);
+
+	
+	}
+}
