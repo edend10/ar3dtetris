249a3726ede69e58c88bee44f093812d36d6ac84
Zeynep Ejder <zeynepejder@dyn-160-39-251-122.dyn.columbia.edu>
2016-05-03 01:34:02 -0400

    PowerUpActivator.cs

---
+++ FinalProject4172/Assets/Scripts/PowerUpActivator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpActivator : MonoBehaviour {
+
+	public GameObject destroyPowerUp;
+
+	void Start () {
+
+	}
+
+	void Update () {
+
+		if (Time.time > 10 && Time.time < 12) {
+			destroyPowerUp.gameObject.SetActive (true);
+		}
+	}
+}
